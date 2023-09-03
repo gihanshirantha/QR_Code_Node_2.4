96c88fd46f4dac50ca5dc6f82b722eac36dc8b88
Gihan Shirantha <shirantha1999@gmail.com>
2023-09-03 18:05:49 +0530

    Generate qr code implementation

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs"
 
 inquirer
     .prompt([
         {
             message:"Type in your URL :",
             name:"URL",
-        }
+        } 
     ])
     .then((answers)=>{
-        console.log(answers);
+        const url=answers.URL;
+        var qr_svg=qr.image(url);
+        qr_svg.pipe(fs.createWriteStream("qr_img.png"))
     })
     .catch((error)=>{
         if(error.isTtyError){
 
         }else{
-            
+
         }
     })
 
